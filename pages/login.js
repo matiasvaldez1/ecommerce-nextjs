5b5a87861278e6368c2b1fd3f71f2de925de3185
Matias Valdez <matiasvaldez8184@gmail.com>
2022-04-16 11:35:20 -0300

    Google

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ export default function Component() {
     <h1 className='select-none no-underline block h-8 text-4xl my-8 text-center dark:text-white'><a className='text-red-500'>SignIn</a></h1>
     <div className="w-6/12 h-[400px] m-auto grid place-content-center">
       <button className="bg-slate-900 text-white p-2 rounded-md hover:opacity-70" onClick={() => signIn("github",{ callbackUrl: `/` })}>Sign in with Github</button>
-      <button  className="bg-cyan-600 text-white p-2 rounded-md hover:opacity-70 my-2"  onClick={() => signIn("google",{ callbackUrl: `/` })}>Sign in with Google</button>
+      <button  className="bg-cyan-600 text-white p-2 rounded-md hover:opacity-70 my-2"  onClick={() => signIn("google",{ callbackUrl: `https://loudrop-store.vercel.app/api/auth/callback/google` })}>Sign in with Google</button>
     </div>
     </>
   )
